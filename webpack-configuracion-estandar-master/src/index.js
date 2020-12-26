3391be3441d3f44758e823f998e8c6c8689fb35e
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2020-12-26 11:26:45 -0400

    Promise all .. Como se trabajan

--- webpack-configuracion-estandar-master/src/index.js
+++ webpack-configuracion-estandar-master/src/index.js
@@ -3,12 +3,14 @@ import { buscarHeroe as buscrHeroeCallback} from './js/callbacks';
 import { buscarHeroes } from './js/promesas';
 
 const heroeId = 'iron';
+const heroeId2 = 'capi';
 
-buscrHeroeCallback(heroeId, (err,heroe)=>{
-    if (heroe){
-        console.log(`Hola ${ heroe.poder }`); 
-    }else{
-        console.log(` ${ err}`); 
-    }
+/* buscarHeroes(heroeId).then(heroe => {
+    console.log(`Enviando heroe ${heroe.nombre} a la mision`);
+})        */   
 
+Promise.all([buscarHeroes(heroeId), buscarHeroes(heroeId2)]).then(([resp1,resp2])=>{
+    console.log(` Enviando  ${resp1.nombre} y a ${resp2.nombre}`)
 });
+
+console.log('Fin de programa');
